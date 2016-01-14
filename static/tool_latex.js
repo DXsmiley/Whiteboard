310ff4e5216206317dc51e07699fabad8c01e6ed
DXsmiley <dxsmiley@hotmail.com>
2016-01-14 13:39:17 +1100

    LaTeX tool initial version

---
+++ static/tool_latex.js
@@ -0,0 +1,105 @@
+(function () {
+
+	jQuery.fn.selectText = function() {
+		var range, selection;
+		return this.each(function() {
+			if (document.body.createTextRange) {
+				range = document.body.createTextRange();
+				range.moveToElementText(this);
+				range.select();
+			} else if (window.getSelection) {
+				selection = window.getSelection();
+				range = document.createRange();
+				range.selectNodeContents(this);
+				selection.removeAllRanges();
+				selection.addRange(range);
+			}
+		});
+	};
+
+	function getLatexURL() {
+		// Todo: better way of getting the LaTeX address.
+		tex = $('#modal_latex_input').text();
+		url = 'http://localhost:5000/i.png?c=' + encodeURIComponent(tex);
+		return url;
+	}
+
+	function LatexHead() {
+		this.position = new Point(0, 0);
+		this.stage = 0;
+		// 0 - text input
+		// 1 - placement
+		modelOpen('.modal_latex');
+		$('#model_latex_input').text('\sqrt{a^2 + b^2} = c');
+		$('#model_latex_input').show();
+		$('#modal_latex_positionable').hide();
+	}
+
+	LatexHead.prototype.onMove = function(p) {
+		this.position = new Point(p.x, p.y);
+		$('#modal_latex_positionable').css('left', p.x);
+		$('#modal_latex_positionable').css('top', p.y);
+	}
+
+	LatexHead.prototype.onRelease = function() {
+		// do nothing...
+	}
+
+	LatexHead.prototype.onModalConfirm = function() {
+		if (this.stage == 1) {
+			console.log('LaTeX confirmed');
+			sendPaintEvent('image', {
+				position: this.position,
+				url: getLatexURL()
+			});
+			this.onModalCancel();
+		}
+		if (this.stage == 0) {
+			this.stage = 1;
+			$('#modal_latex_input').hide();
+			$('#modal_latex_image').hide();
+			$('#modal_latex_positionable').show();
+		}
+	}
+
+	LatexHead.prototype.onModalCancel = function() {
+		modelClose('.modal_latex');
+	}
+
+	var latex_get_timeout = null;
+
+	$(document).ready(function() {
+		// When a key is pressed, update the image to reflect the code.
+		$('#modal_latex_input').keydown(function() {
+			if (latex_get_timeout != null) {
+				window.clearTimeout(latex_get_timeout);
+			}
+			latex_get_timeout = window.setTimeout(
+				function() {
+					console.log('Updating LaTeX preview:', getLatexURL());
+					$('#modal_latex_image').attr('src', getLatexURL());
+					$('#modal_latex_positionable').attr('src', getLatexURL());
+				}
+			, 400);
+		});
+	});
+
+	makeTool({
+		name: 'latex',
+		buttonImage: 'col_white.png',
+		buttonImageSelected: 'col_s_white.png',
+		onButtonClick: function() {
+			console.log('LaTeX!');
+			return true;
+		},
+		makeToolHead: function() {
+			return new LatexHead();
+		},
+		drawFull: function(data) {
+			// This should never happen.
+			// Draw calls should be routed to the image tool.
+			console.error('Called drawFull on LaTeX tool.');
+		}
+	});
+
+})();
--- templates/whiteboard.tpl
+++ templates/whiteboard.tpl
@@ -19,6 +19,7 @@
 				<img id="button_eraser" src="/static/images/eraser.png"><br>
 				<img id="button_text"   src="/static/images/text.png"><br>
 				<img id="button_image"  src="/static/images/button_image.png"><br>
+				<img id="button_latex"  src="/static/images/col_white.png"><br>
 				<img id="button_clear"  src="/static/images/clear.png"><br>
 				<img id="colour_blue"   src="/static/images/col_blue.png"><br>
 				<img id="colour_red"    src="/static/images/col_red.png"><br>
@@ -40,6 +41,13 @@
 				<div class="modal_image">
 					<img id="modal_image" src="/static/images/placeholder.png" class="modal_image" />
 				</div>
+				<div class="modal_latex">
+					<center>
+						<p contenteditable id="modal_latex_input" class="modal_latex">\sqrt{a^2 + b^2} = c</p>
+						<img id="modal_latex_image" class="modal_latex" src="http://localhost:5000/i.png?c=%5Csqrt%7Ba%5E2%20%2B%20b%5E2%7D%20%3D%20c">
+					</center>
+					<img id="modal_latex_positionable" class="modal_latex" src="http://localhost:5000/i.png?c=%5Csqrt%7Ba%5E2%20%2B%20b%5E2%7D%20%3D%20c">
+				</div>
 			</div>
 		</div>
 	</body>
@@ -55,6 +63,7 @@
 	<script type="text/javascript" src="/static/tool_clear.js"></script>
 	<script type="text/javascript" src="/static/tool_text.js"></script>
 	<script type="text/javascript" src="/static/tool_image.js"></script>
+	<script type="text/javascript" src="/static/tool_latex.js"></script>
 	<script type="text/javascript" src="/static/whiteboard.js"></script>
 	<script type="text/javascript">setWhiteboardId("{{board_id}}");</script>
 
