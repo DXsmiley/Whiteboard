0e76e5d931cf4366e31b7c41aad2b144b9f75dad
DXsmiley <dxsmiley@hotmail.com>
2016-03-02 20:52:33 +1100

    Rewrote undo tool

--- static/tool_undo.js
+++ static/tool_undo.js
@@ -1,23 +1,16 @@
-(function() {
+function UndoTool() {
+	this.name = 'undo';
+	this.image = 'button_undo.png',
+}
 
-	makeTool({
-		name: 'undo',
-		buttonImage: 'button_undo.png',
-		onButtonClick: function() {
-			if (paint_blobs_mine.length > 0) {
-				var uid = paint_blobs_mine.pop();
-				console.log('Undoing: ', uid, paint_blobs_mine.length);
-				sendUndoEvent(uid);
-				drawEverything();
-			} else {
-				console.log('Nothing to undo');
-			}
-			return false;
-		},
-		drawFull: function(data) {
-			console.warn('Drawing undo event. This shouldn\'t be a thing')
-		}
-
-	});
-
-})();
+UndoTool.prototype.onButtonClick = function() {
+	if (paint_blobs_mine.length > 0) {
+		var uid = paint_blobs_mine.pop();
+		console.log('Undoing: ', uid, paint_blobs_mine.length);
+		sendUndoEvent(uid);
+		drawEverything();
+	} else {
+		console.log('Nothing to undo');
+	}
+	return false;
+};
