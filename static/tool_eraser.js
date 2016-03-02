da39ce82838311529742a53a20ce8e92441e8aab
DXsmiley <dxsmiley@hotmail.com>
2016-03-02 20:49:23 +1100

    Rewrote eraser tool

--- static/tool_eraser.js
+++ static/tool_eraser.js
@@ -1,14 +1,19 @@
-makeTool({
-	name: 'eraser',
-	buttonImage: 'eraser.png',
-	buttonImageSelected: 'eraser_select.png',
-	onButtonClick: function() {
-		return true;
-	},
-	makeToolHead: function() {
-		return new PencilHead('eraser', '#dddddd', 30);
-	},
-	drawFull: function(data) {
-		drawLine(data.points, context_picture, '#ffffff', 30);
-	}
-});
+function EraserTool() {
+	this.name = 'eraser';
+	this.buttonImage = 'eraser.png';
+	this.buttonImageSelected = 'eraser_select.png';
+}
+
+EraserTool.prototype.onButtonClick = function() {
+	return true;
+};
+
+EraserTool.prototype.makeToolHead = function() {
+	return new PencilHead('eraser', '#dddddd', 30, 'straight');
+};
+
+EraserTool.prototype.drawFull = function(data) {
+	drawLine(data.points, context_picture, '#ffffff', 30);
+};
+
+makeTool(new EraserTool());
