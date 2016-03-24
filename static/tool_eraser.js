adfc66ed938af1b32cd621e762d8e84e14f48cbf
DXsmiley <dxsmiley@hotmail.com>
2016-03-24 12:53:55 +1100

    Eraser path is no longer grey

--- static/tool_eraser.js
+++ static/tool_eraser.js
@@ -9,7 +9,7 @@ EraserTool.prototype.onButtonClick = function() {
 };
 
 EraserTool.prototype.makeToolHead = function() {
-	return new PencilHead('eraser', '#dddddd', 30, 'straight');
+	return new PencilHead('eraser', '#ffffff', 30, 'straight');
 };
 
 EraserTool.prototype.drawFull = function(data) {
