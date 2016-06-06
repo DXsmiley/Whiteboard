d3604720585d818e4cfc161ab10799a422b9883a
DXsmiley <dxsmiley@hotmail.com>
2016-06-06 17:37:56 +1000

    Indent with tabs
    Removed inferior spaces

--- database.py
+++ database.py
@@ -8,22 +8,22 @@ database = client[sys.argv[3]]
 collection = database['whiteboards']
 
 def action_push(bid, action, time):
-    collection.update({'_id': bid}, {'$push': {'layers': action}, '$set': {'timestamp': time}}, upsert = True)
+	collection.update({'_id': bid}, {'$push': {'layers': action}, '$set': {'timestamp': time}}, upsert = True)
 
 def action_remove(bid, action_id, time):
-    collection.update({'_id': bid}, {'$pull': {'layers': {'action_id': action_id}}, '$set': {'timestamp': time}}, upsert = True)
+	collection.update({'_id': bid}, {'$pull': {'layers': {'action_id': action_id}}, '$set': {'timestamp': time}}, upsert = True)
 
 def rewrite(bid, data):
-    collection.update({'_id': bid}, data, upsert = True)
-    
+	collection.update({'_id': bid}, data, upsert = True)
+
 def load(bid):
-    return collection.find_one({'_id': bid})
+	return collection.find_one({'_id': bid})
 
 def load_meta():
-    results = []
-    for i in collection.find({}, {'_id': True, 'timestamp': True}):
-        results.append({
-            'name': i['_id'],
-            'timestamp': i.get('timestamp', 0)
-        })
-    return results
+	results = []
+	for i in collection.find({}, {'_id': True, 'timestamp': True}):
+		results.append({
+			'name': i['_id'],
+			'timestamp': i.get('timestamp', 0)
+		})
+	return results
