e816c3a3b3d90fd81d779b85e8e61cf2010a52a3
DXsmiley <dxsmiley@hotmail.com>
2016-06-25 12:29:21 +1000

    Removed the internal feedback system

--- database.py
+++ database.py
@@ -8,49 +8,32 @@ enable = settings.get('db_enable')
 if enable:
 	client = pymongo.MongoClient(settings.get('db_login'))
 	database = client[settings.get('db_name')]
-	whiteboards = database['whiteboards']
-	feedback = database['feedback']
+	collection = database['whiteboards']
 else:
 	print('Warning: Database not enabled')
 
 def action_push(bid, action, time):
 	if enable:
-		whiteboards.update({'_id': bid}, {'$push': {'layers': action}, '$set': {'timestamp': time}}, upsert = True)
+		collection.update({'_id': bid}, {'$push': {'layers': action}, '$set': {'timestamp': time}}, upsert = True)
 
 def action_remove(bid, action_id, time):
 	if enable:
-		whiteboards.update({'_id': bid}, {'$pull': {'layers': {'action_id': action_id}}, '$set': {'timestamp': time}}, upsert = True)
+		collection.update({'_id': bid}, {'$pull': {'layers': {'action_id': action_id}}, '$set': {'timestamp': time}}, upsert = True)
 
 def rewrite(bid, data):
 	if enable:
-		whiteboards.update({'_id': bid}, data, upsert = True)
+		collection.update({'_id': bid}, data, upsert = True)
 
 def load(bid):
 	if enable:
-		return whiteboards.find_one({'_id': bid})
+		return collection.find_one({'_id': bid})
 
 def load_meta():
 	results = []
 	if enable:
-		for i in whiteboards.find({}, {'_id': True, 'timestamp': True}):
+		for i in collection.find({}, {'_id': True, 'timestamp': True}):
 			results.append({
 				'name': i['_id'],
 				'timestamp': i.get('timestamp', 0)
 			})
 	return results
-
-def feedback_post(message):
-	if enable:
-		feedback.insert({'message': message})
-
-def feedback_delete(feedback_id):
-	if enable:
-		feeedback.remove({'_id': feedback_id})
-
-def feedback_list():
-	if enable:
-		for i in feedback.find():
-			yield {
-				'id': i['_id'],
-				'message': i['message']
-			}
--- templates/send.tpl
+++
@@ -1,8 +0,0 @@
-{% extends "info.tpl" %}
-{% block title %}Whiteboard - Feedback{% endblock %}
-{% block content %}
-	<div class="container">
-		<h2>Thanks for the feedback!</h2>
-		<h1>&#x2764;</h1>
-	</div>
-{% endblock %}
